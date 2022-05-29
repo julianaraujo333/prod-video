636e7dbc0359ea1656356d9178fd99a8e1114c8c
Juliana Araujo <julianaraujo555@outlook.com>
2022-05-29 14:03:56 -0300

    adding video and fullscreen hook

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -1,22 +1,51 @@
+import { useRef } from "react";
 import styles from "./Video.module.css";
+import useFullscreenStatus from "../../hooks/useFullScreen";
 import { Link } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 import { BiRightArrow } from "react-icons/bi";
 import { BiLeftArrow } from "react-icons/bi";
-function Menu({ next }) {
+import video from "./../../Assets/video.mp4";
+function Menu() {
+  const videoElem = useRef(null);
+
+  let isFullScreen, setIsFullScreen;
+  let errorMsg;
+
+  try {
+    [isFullScreen, setIsFullScreen] = useFullscreenStatus(videoElem);
+  } catch (e) {
+    errorMsg = "Fullscreen not supported";
+    isFullScreen = false;
+    setIsFullScreen = undefined;
+  }
+
+  const handleExitFullScreen = () => document.exitFullscreen();
+
   return (
     <div className={styles.wrapper}>
-      <Link to="/">
-        <AiFillHome />
-      </Link>
+      <div className={styles.videoContent}>
+        <video controls autoPlay={true}>
+          <source src={video} type="video/mp4" ref={videoElem} />
+        </video>
+      </div>
 
-      <div>
+      <div className={styles.menu}>
         <Link to="/">
-          <BiLeftArrow />
+          <AiFillHome />
         </Link>
-        <Link to="/question">
-          <BiRightArrow />
-        </Link>
+        <button onClick={isFullScreen ? handleExitFullScreen : setIsFullScreen}>
+          {isFullScreen ? "Minimizar tela" : "Assistir em tela cheia"}
+        </button>
+
+        <div>
+          <Link to="/">
+            <BiLeftArrow />
+          </Link>
+          <Link to="/question">
+            <BiRightArrow />
+          </Link>
+        </div>
       </div>
     </div>
   );
